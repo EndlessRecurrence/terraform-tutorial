cb72c096d9d97d948e490cf0e23db7cd263c733d
EndlessRecurrence <georgesut@yahoo.com>
2023-07-31 10:25:33 +0300

    Add VPCs and subnets

--- main.tf
+++ main.tf
@@ -12,10 +12,43 @@ provider "aws" {
   secret_key = "a4auqEWb5YNJDSBzQM3FzNg6FulL8ZROfwVCccD8"
 }
 
-resource "aws_instance" "my-first-instance" {
-  ami           = "ami-0ab1a82de7ca5889c"
-  instance_type = "t2.micro"
+resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "Ubuntu"
+    Name = "production"
   }
 }
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+resource "aws_vpc" "second-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "Dev"
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  vpc_id     = aws_vpc.second-vpc.id
+  cidr_block = "10.1.1.0/24"
+
+  tags = {
+    Name = "dev-subnet"
+  }
+}
+
+# resource "aws_instance" "my-first-instance" {
+#   ami           = "ami-0ab1a82de7ca5889c"
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "Ubuntu"
+#   }
+# }
+
